4c45f25d6eb369bce951d7af293236bf7fc1aaf4
Andrea Della Corte <andrea@dellacorte.me>
2017-09-12 02:09:05 +0300

    Moving the Unity folder since UNITY_HOME doesn't seem to work

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -7,16 +7,14 @@ set +e
 mkdir -p ~/.improbable/oauth2
 mv ./secret ~/.improbable/oauth2/oauth2_refresh_token
 
-UNITY_HOME=/Applications/Unity-5.6.1f1
+mv /Applications/Unity-5.6.1f1 /Applications/Unity
 
 assembly_name=MySimpleRPG_$( date +%Y%m%d_%H%M%S )
 
-spatial update
+spatial worker build --target=deployment
 
-spatial worker build --target=deployment --log_level=debug
+spatial cloud upload $assembly_name
 
-spatial cloud upload $assembly_name --log_level=debug
-
-spatial cloud launch $assembly_name default_launch.json beta_batman_crazy_339 --snapshot=snapshots/default.snapshot --cluster_region=eu --log_level=debug
+spatial cloud launch $assembly_name default_launch.json beta_batman_crazy_339 --snapshot=snapshots/default.snapshot --cluster_region=eu
 
 set -e
