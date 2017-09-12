770668243d877c3aa726001484682d3ed22b3265
Andrea Della Corte <andrea@dellacorte.me>
2017-09-12 09:06:49 +0300

    Taking a look at the build logs and verifying cache works

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -7,6 +7,9 @@ set +e
 mkdir -p ~/.improbable/oauth2
 mv ./secret ~/.improbable/oauth2/oauth2_refresh_token
 
+# This would be better, but it doesn't work for some reason
+#UNITY_HOME=/Applications/Unity-5.6.1f1
+
 mv /Applications/Unity-5.6.1f1 /Applications/Unity
 
 assembly_name=MySimpleRPG_$( date +%Y%m%d_%H%M%S )
@@ -16,5 +19,7 @@ spatial worker build --target=deployment
 spatial cloud upload $assembly_name
 
 spatial cloud launch $assembly_name default_launch.json beta_batman_crazy_339 --snapshot=snapshots/default.snapshot --cluster_region=eu
+
+cat /Users/travis/build/andreadellacorte/SimpleRPG/logs/spatial.log
 
 set -e
