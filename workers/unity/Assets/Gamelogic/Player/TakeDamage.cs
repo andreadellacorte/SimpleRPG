d0d5d6664d9278412789974a5a06423e7f0590e8
Andrea Della Corte <andrea@dellacorte.me>
2017-09-16 12:19:18 +0300

    Clean extra logs and do not cache Homebrew

--- workers/unity/Assets/Gamelogic/Player/TakeDamage.cs
+++ workers/unity/Assets/Gamelogic/Player/TakeDamage.cs
@@ -12,8 +12,6 @@ namespace Assets.Gamelogic.Player
         [Require] private Health.Writer HealthWriter;
 
         private void OnTriggerEnter(Collider other) {
-
-            Debug.LogError("Trigger Enter");
             /*
              * Unity's OnTriggerEnter runs even if the MonoBehaviour is disabled, so non-authoritative UnityWorkers
              * must be protected against null writers
@@ -25,19 +23,13 @@ namespace Assets.Gamelogic.Player
             if (HealthWriter.Data.health <= 0)
                 return;
 
-            Debug.Log("Other tag: " + other.gameObject.tag);
-
             if (other != null && other.gameObject.CompareTag("Sword")) {
-
-                Debug.LogError("Sword");
                 // Reduce health of this entity when hit
                 int newHealth = HealthWriter.Data.health - 250;
                 HealthWriter.Send(new Health.Update().SetHealth(newHealth));
 
                 //Color.Lerp(Color.white, Color.red, Mathf.PingPong(Time.time, 1));
             }
-
-            Debug.LogWarning("Collision Exit");
         }
     }
 }
