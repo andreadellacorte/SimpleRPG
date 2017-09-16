605f53ec15a9902befbe7f3b80ea969eb42a0b5a
Andrea Della Corte <andrea@dellacorte.me>
2017-09-16 12:54:55 +0300

    Move deployment to a separate script

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -3,9 +3,3 @@
 set -e
 
 spatial worker build --target=deployment
-
-spatial cloud upload $ASSEMBLY_NAME
-
-spatial cloud delete $PROJECT_NAME || true
-
-spatial cloud launch $ASSEMBLY_NAME default_launch.json $ASSEMBLY_NAME --snapshot=snapshots/default.snapshot --cluster_region=eu
---
+++ Scripts/deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+spatial cloud delete $PROJECT_NAME || true
+
+spatial cloud upload $ASSEMBLY_NAME
+
+spatial cloud launch $ASSEMBLY_NAME default_launch.json $ASSEMBLY_NAME --snapshot=snapshots/default.snapshot --cluster_region=eu
