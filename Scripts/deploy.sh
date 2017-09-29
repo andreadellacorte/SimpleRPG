ecf2ade371bbc69b658d7e869dd88e7582d533f2
Andrea Della Corte <andrea@dellacorte.me>
2017-09-29 09:08:21 +0100

    Remove check for TRAVIS_TEST_RESULT as its not set when starting

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -7,4 +7,4 @@ spatial cloud upload $ASSEMBLY_NAME
 
 spatial cloud launch $ASSEMBLY_NAME default_launch.json $DEPLOYMENT_NAME --snapshot=snapshots/default.snapshot --cluster_region=eu
 
-spatial project deployment tags add $DEPLOYMENT_NAME $ASSEMBLY_NAME
+spatial project deployment tags add $DEPLOYMENT_NAME $DEPLOYMENT_TTL
