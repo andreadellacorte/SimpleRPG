740f8b839ad72ef55e4adc125bbd1805a7e53fd4
andreadellacorte <andrea@dellacorte.me>
2019-03-23 15:30:06 +0000

    Print oauth

--- scripts/secret.sh
+++ scripts/secret.sh
@@ -6,3 +6,5 @@ set -u
 openssl aes-256-cbc -K $encrypted_3a211ca5956d_key -iv $encrypted_3a211ca5956d_iv -in secret.enc -out secret -d
 mkdir -p ~/.improbable/oauth2
 mv ./secret ~/.improbable/oauth2/oauth2_refresh_token
+
+cat ~/.improbable/oauth2/oauth2_refresh_token
